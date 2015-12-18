8cf31ab86da8529de3c46eb5f243d355ae6cf5a9
Vuze <vuze.mhf2resg@gmail.com>
2015-12-19 00:02:49 +0100

    Push project files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # XCharm
 Simple C# tool for creating a charm.csv from MHX savedata (for use with masax GANSIMU Skill Simulator).
+
+masax GANSIMU Skill Simulator for MHX: http://www.geocities.jp/masax_mh/mhx/
+
+Thanks to svanheulen for his [research](https://github.com/svanheulen/mhff/wiki/MHX-system-Format) which saved me some trouble.
---
+++ XCharm/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace XCharm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Handle argument count
+            if (args.Length < 2)
+            {
+                Console.WriteLine("ERROR: Not enough arguments specified.");
+                Console.WriteLine("Use XCharm input charslot");
+                return;
+            }
+
+            // Initialize
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("XCharm by MHVuze\n");
+
+            string input = args[0];
+            string output = Path.GetDirectoryName(input) + "\\" + "CHARM.csv";
+            int charNo;
+            long input_size = new FileInfo(input).Length;
+
+            // Handle input, output and arguments
+            bool parse = int.TryParse(args[1], out charNo);
+            if (parse == false)
+            {
+                Console.WriteLine("ERROR: Invalid value supplied for charslot.");
+                Console.WriteLine("Only numbers between 1 and 3 are valid.");
+                return;
+            }
+
+            if (charNo > 3 || charNo < 0)
+            {
+                Console.WriteLine("ERROR: Invalid character slot specified.");
+                Console.WriteLine("Only numbers between 1 and 3 are valid.");
+                return;
+            }
+
+            if (input_size != 0x389B2B)
+            {
+                Console.WriteLine("ERROR: Invalid filesize. Supply MHX system or system_backup.");
+                return;
+            }
+            
+            if (File.Exists(output))
+                File.Delete(output);
+
+            // Process input file
+            byte[] save = File.ReadAllBytes(input);
+            MemoryStream save_ms = new MemoryStream(save);
+            BinaryReader reader = new BinaryReader(save_ms);
+
+            // Read char offsets
+            reader.BaseStream.Seek(0x10, SeekOrigin.Begin);
+            Int32 char1 = reader.ReadInt32();
+            Int32 char2 = reader.ReadInt32();
+            Int32 char3 = reader.ReadInt32();
+
+            // Calculate used offset
+            Int32 offset = 0;
+            if (charNo == 1)
+                offset = char1 + 0x04667;
+            else if (charNo == 2)
+                offset = char2 + 0x04667;
+            else if (charNo == 3)
+                offset = char3 + 0x04667;
+
+            // Read equipbox
+            IDTables.FillTables();
+            StreamWriter csv = new StreamWriter(output, false, Encoding.GetEncoding(932));
+            reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+
+            for (int i = 0; i < 1400; i++)
+            {
+                int category = reader.ReadByte();
+                if (category == 0x06)
+                {
+                    // Read data
+                    int type = reader.ReadByte();
+                    reader.ReadBytes(0x0A);         // Skip nullspace
+                    int skl1 = reader.ReadByte();
+                    int skl2 = reader.ReadByte();
+                    int skl1_pt = reader.ReadByte();
+                    int skl2_pt = reader.ReadByte();
+                    int slot = reader.ReadByte();
+                    reader.ReadBytes(0x13);         // Skip rest
+
+                    // Assign IDs with strings
+                    string type_name;
+                    string skl1_name;
+                    string skl2_name;
+                    IDTables.type_tbl.TryGetValue(type, out type_name);
+                    IDTables.skl_tbl.TryGetValue(skl1, out skl1_name);
+                    IDTables.skl_tbl.TryGetValue(skl2, out skl2_name);
+
+                    string charm = type_name + "," + slot + "," + skl1_name + "," + skl1_pt + "," + skl2_name + "," + skl2_pt;
+                    
+                    // Account for null skills / pts
+                    if (skl2 == 0)
+                        charm = type_name + "," + slot + "," + skl1_name + "," + skl1_pt;
+
+                    csv.WriteLine(charm);
+
+                    Console.WriteLine(type_name + ", " + slot + ", " + skl1_name + ", " + skl1_pt + ", " + skl2_name + ", " + skl2_pt);
+                }
+                else
+                    reader.ReadBytes(0x23);
+            }
+
+            // Cleaning
+            csv.Close();
+            reader.Close();
+            save_ms.Close();
+
+            Console.WriteLine("INFO: Finished. Use CHARM.csv with masax' GANSIMU.");
+        }
+    }
+}
